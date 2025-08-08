eaef8559077bc998b9c096371555894c14d290c5
vigneshkumarm140302 <vigneshkumarm140302@gmail.com>
2025-08-08 19:54:28 +0530

    access token reset

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
@@ -18,20 +19,44 @@ export const AuthProvider = ({ children }) => {
     baseURL: backendUrl,
   });
 
-  api.interceptors.request.use(async (config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-
-  
-  } ,async (error) => {
-      return Promise.reject(error)
+  const getNweToken = async () => {
+    try {
+      const responce = await axios.post(`${backendUrl}/api/token/refresh/`, {
+        refresh: refreshToken,
+      });
+      if (responce.status === 200) {
+        setAccessToken(responce.data.access);
+        localStorage.setItem("access_token", responce.data.access);
+        return responce.data.access;
+      } else {
+        console.log(responce);
+      }
+    } catch (error) {
       console.log(error);
-      
-    });
+    }
+  };
+  api.interceptors.request.use(
+    async (config) => {
+      const token = localStorage.getItem("access_token");
+
+      if (token) {
+        const decode = jwtDecode(token);
+        const now = Date.now() / 1000;
+        if (decode.exp < now) {
+          token = await getNweToken();
+        }
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+
+      return config;
+    },
+    async (error) => {
+      return Promise.reject(error);
+      console.log(error);
+    }
+  );
 
   const value = { backendUrl, navigate, api };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
